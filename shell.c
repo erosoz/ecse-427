604ae267ba31e8ff2246c70f6852d9af921b1170
erosoz <121735758+erosoz@users.noreply.github.com>
2026-02-26 19:56:34 +0000

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
-#include <unistd.h>
+#include <ctype.h>              // isspace
+#include <string.h>
+#include <unistd.h>             // isatty
 #include "shell.h"
 #include "interpreter.h"
 #include "shellmemory.h"
@@ -12,26 +13,34 @@ int parseInput(char ui[]);
 int main(int argc, char *argv[]) {
     printf("Shell version 1.5 created Dec 2025\n");
 
-    char prompt = '$';  				// Shell prompt
-    char userInput[MAX_USER_INPUT];		// user's input stored here
-    int errorCode = 0;					// zero means no error, default
+    char prompt = '$';          // Shell prompt
+    char userInput[MAX_USER_INPUT];     // user's input stored here
+    // batch_mode is true when a file was given.
+    int batch_mode = !isatty(STDIN_FILENO);
+    int errorCode = 0;          // zero means no error, default
 
     //init user input
     for (int i = 0; i < MAX_USER_INPUT; i++) {
         userInput[i] = '\0';
     }
-    
+
     //init shell memory
     mem_init();
-    while(1) {
-	if (isatty(STDIN_FILENO)) { //checks the mode type, if it is interactive prints the $							
-        printf("%c ", prompt);
-	}
-	if (fgets(userInput, MAX_USER_INPUT-1, stdin) == NULL) { //so that it doesnt loop if there is no command left it breaks from loop/reading
-		break;
-}
+    while (1) {
+        if (!batch_mode) {
+            printf("%c ", prompt);
+        }
+        // here you should check the unistd library 
+        // so that you can find a way to not display $ in the batch mode
+        fgets(userInput, MAX_USER_INPUT - 1, stdin);
         errorCode = parseInput(userInput);
-        if (errorCode == -1) exit(99);	// ignore all other errors
+        if (errorCode == -1)
+            exit(99);           // ignore all other errors
+
+        if (feof(stdin)) {
+            return 0;
+        }
+
         memset(userInput, 0, sizeof(userInput));
     }
 
@@ -41,32 +50,65 @@ int main(int argc, char *argv[]) {
 int wordEnding(char c) {
     // You may want to add ';' to this at some point,
     // or you may want to find a different way to implement chains.
-    return c == '\0' || c == '\n' || c == ' ' || c == ';' ;
+    return c == '\0' || c == '\n' || isspace(c) || c == ';';
 }
 
 int parseInput(char inp[]) {
-    char tmp[200], *words[100];                            
+    char tmp[200], *words[100];
     int ix = 0, w = 0;
     int wordlen;
-    int errorCode;
-    while (inp[ix] != '\n' && inp[ix] != '\0') {
-    w = 0;
-    for (; inp[ix] == ' ' && ix < 1000; ix++); // skip white spaces
-    while (inp[ix] != ';' && ix < 1000 && inp[ix] != '\n' && inp[ix] != '\0') {
+    int errorCode = 0;
+
+    // This function probably isn't the best place to handle chains.
+    // That is, if we really wanted to implement relatively complex
+    // syntax like that of bash, we should really just tokenize everything,
+    // send the ';' as a separate word to the interpreter, and let the
+    // interpreter sort it out later.
+    // But for this simple shell, the interpreter's job is really only to be
+    // command dispatch, and this function is really acting as a complete
+    // parser rather than just a tokenizer. So we'll handle it here.
+
+    while (inp[ix] != '\n' && inp[ix] != '\0' && ix < 1000) {
+        // skip white spaces
+        for (; isspace(inp[ix]) && inp[ix] != '\n' && ix < 1000; ix++);
+
+        // If the next character is a semicolon,
+        // we should run what we have so far.
+        if (inp[ix] == ';')
+            break;
+
         // extract a word
         for (wordlen = 0; !wordEnding(inp[ix]) && ix < 1000; ix++, wordlen++) {
-            tmp[wordlen] = inp[ix];                        
+            tmp[wordlen] = inp[ix];
         }
-        tmp[wordlen] = '\0';
-        words[w] = strdup(tmp);
-        w++;
-        if (inp[ix] == '\0' || inp[ix] == '\n' || inp[ix] == ';') break;
 
-  
-    while (inp[ix] == ' ' && ix < 1000) ix++;
+        if (wordlen > 0) {
+            tmp[wordlen] = '\0';
+            words[w] = strdup(tmp);
+            w++;
+            if (inp[ix] == '\0')
+                break;
+        } else {
+            break;
+        }
     }
-    errorCode = interpreter(words, w);
-    ix++;
+    // Ignore commands that contain no (meaningful) input by only calling the
+    // interpreter if actually found words.
+    if (w > 0) {
+        errorCode = interpreter(words, w);
+        for (size_t i = 0; i < w; ++i) {
+            free(words[i]);
+        }
+    }
+    if (inp[ix] == ';') {
+        // handle the next command in the chain by recursing
+        // the parser. We could equivalently wrap all of the work above
+        // in a while loop, but this makes it clearer what's going on.
+        // Additionally, a modern compiler is more than smart enough to
+        // turn this into a loop for us! Try adding -O2 to the CFLAGS in
+        // the Makefile and then read the assembly we get.
+        // Or you might be interested in godbolt.org.
+        return parseInput(&inp[ix + 1]);
     }
     return errorCode;
 }
